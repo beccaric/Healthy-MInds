58601484d7b838e100e2daa9aa1122ef7ce618dc
beccaric <bec.richards95@gmail.com>
2021-12-06 19:50:56 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,12 @@ alert("Successfully Subscribed!");
 return true;
 }
 
+
+// JS for Contact Us page 
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
